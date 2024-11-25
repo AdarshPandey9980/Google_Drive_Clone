2b7b1b0115b7ebad9c3e438fc777c4d310db16ff
AdarshPandey9980 <stud9980y@gmail.com>
2024-11-25 21:20:50 +0530

    card and some navigation part done

---
+++ components/Card.tsx
@@ -0,0 +1,30 @@
+import Link from 'next/link'
+import { Models } from 'node-appwrite'
+import React from 'react'
+import Thumbnail from './Thumbnail'
+import { convertFileSize } from '@/lib/utils'
+import FormatedDateAndTime from './FormatedDateAndTime'
+import ActionDropDown from './ActionDropDown'
+
+const Card = ({file}: {file:Models.Document}) => {
+  return (
+    <Link href={file.url} target='_blank' className='file-card'>
+        <div className='flex justify-between'>
+            <Thumbnail type={file.type} extension={file.extension} url={file.url} className='!size20' />
+            <div className='flex flex-col justify-between items-end'>
+            <ActionDropDown/>
+            <p className='body-1 '>{convertFileSize(file.size)}</p>
+        </div>
+        </div>
+        
+        <div className='file-card-details'>
+            <p className='subtitle-2 line-clamp-1'>{file.name}</p>
+            <FormatedDateAndTime date={file.$createdAt} className="body-2 text-light-100"/>
+            <p className='caption line-clamp-1 text-light-200'>{file.owner.fullName}</p>
+        </div>
+
+    </Link>
+  )
+}
+
+export default Card
--- lib/actions/file.action.ts
+++ lib/actions/file.action.ts
@@ -3,9 +3,23 @@
 import { createAdminClient } from "../appwrite";
 import {InputFile} from "node-appwrite/file"
 import { appwriteConfig } from "../appwrite/config";
-import { ID } from "node-appwrite";
+import { ID, Models, Query } from "node-appwrite";
 import { constructFileUrl, getFileType, parseStringify } from "../utils";
 import { revalidatePath } from "next/cache";
+import { getCurrentUser } from "./user.action";
+import { error } from "console";
+
+
+const createQueries = (currentUser : Models.Document) => {
+    const queries = [
+        Query.or([
+            Query.equal('owner', [currentUser.$id]),
+            Query.contains('users', [currentUser.email])
+        ])
+    ]
+
+    return queries
+} 
 
 const handleError = (error: any, message: string) => {
     console.log(error, message);
@@ -49,5 +63,29 @@ export const uploadFile =async ({file,ownerId,accountId,path}:UploadFileProps) =
         return parseStringify(newFile)
     } catch (error) {
         handleError(error,"error occured in uploading the file")
+    }
+}
+
+export const getFile = async () => {
+    const {database} = await createAdminClient()
+    try {
+        const currentUser = await getCurrentUser()
+        if (!currentUser) {
+            throw new Error("User not found")
+        }
+
+        const queries = createQueries(currentUser)
+
+        const files = await database.listDocuments(appwriteConfig.databaseId,appwriteConfig.fileCollectionId,
+            queries
+        )
+
+        if (!files) {
+            throw new Error("failed to get the files")
+        }
+
+        return parseStringify(files)
+    } catch (error) {
+        handleError(error,"failed to get files")
     }
 }
