f70afb7c8765c4df54923f5d8498d0009cad3e7b
AdarshPandey9980 <stud9980y@gmail.com>
2024-11-27 14:35:50 +0530

    aa

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,11 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
+
+  typescript:{
+    ignoreBuildErrors: true
+  },
+  
   experimental: {
     serverActions: {
       bodySizeLimit: "100MB"
