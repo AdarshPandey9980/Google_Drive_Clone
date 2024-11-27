c88c0d051a034c04c4766223d201c2f701fa2f3a
AdarshPandey9980 <stud9980y@gmail.com>
2024-11-27 14:28:23 +0530

    done

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -1,5 +1,3 @@
-
-
 import Header from '@/components/Header'
 import MobileNav from '@/components/MobileNav'
 import SideBar from '@/components/SideBar'
@@ -8,15 +6,14 @@ import {  redirect } from 'next/navigation'
 import React from 'react'
 import { Toaster } from "@/components/ui/toaster"
 
+export const dynamic = "force-dynamic";
+
 const layout = async ({children}:{children: React.ReactNode}) => {
 
    const currentUser = await getCurrentUser()
 
-
   return (
-   
-    currentUser ? ( <main className='flex h-screen'>
-      
+    currentUser ? ( <main className='flex h-screen'> 
       <SideBar {...currentUser} /> 
        <section className='flex h-full flex-col flex-1'>
           <MobileNav {...currentUser}/>
@@ -25,9 +22,7 @@ const layout = async ({children}:{children: React.ReactNode}) => {
           <div className='main-content'>{children}</div>
        </section>
        <Toaster />
-  </main>) : (redirect('/sign-in'))
-   
-  )
+  </main>) : (redirect('/sign-in')))
 }
 
 export default layout
